ef97456619e486e9565f730f6ba42419171bb0ab
Sherine.Awad <sherine.awad@gmail.com>
2015-02-11 16:29:53 -0500

    add Mapping-to-Raw scripts

---
+++ scripts/Mapping-to-Raw/idba-digi-extract.sh
@@ -0,0 +1,2 @@
+bash extract-bam.sh /mnt/scratch/tg/mahmoud4/paired-mapping/idba/idba-digi.bam > idba/idba-digi.fasta
+
---
+++ scripts/Mapping-to-Raw/idba-qc-bam.sh
@@ -0,0 +1,3 @@
+
+module load samtools
+~/samtools-1.1/samtools import idba-quality-assembly.fa.fai /mnt/scratch/tg/mahmoud4/paired-mapping/idba/idba-qc.sam /mnt/scratch/tg/mahmoud4/paired-mapping/idba/idba-qc.bam
