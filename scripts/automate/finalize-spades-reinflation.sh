e9115804df0082ab22a81410864435569733c124
Sherine.Awad <sherine.awad@gmail.com>
2015-06-11 21:52:46 -0400

    update makefile

--- scripts/automate/finalize-spades-reinflation.sh
+++
@@ -1,7 +0,0 @@
-GROUPLIST=$(ls -1 kak.group*.pe.fq.gz | cut -c 5-13)
-for g in $GROUPLIST
-do
-   python /usr/local/share/khmer/sandbox/calc-best-assembly.py  *.${g}.*spades.d/contigs.fasta  -o spades-reinflation.best.$g.fa
-done
-
-python /usr/local/share/khmer/sandbox/multi-rename.py assembly  spades-reinflation.best.group*.fa > spades-reinflation.assembly.fa
--- scripts/automate/finalize-velvet-reinflation.sh
+++
@@ -1,7 +0,0 @@
-GROUPLIST=$(ls -1 kak.group*.pe.fq.gz | cut -c 5-13)
-for g in $GROUPLIST
-do
-   python /usr/local/share/khmer/sandbox/calc-best-assembly.py *.${g}.*velvet.*.d/contigs.fa  -o velvet-reinflation-best.$g.fa
-done
-
-python /usr/local/share/khmer/sandbox/multi-rename.py assembly velvet-reinflation-best.group*.fa > velvet-reinflation-assembly.fa
