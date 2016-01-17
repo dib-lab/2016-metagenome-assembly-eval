d777342a54624e4214463a3ab03432c13820adf2
SherineAwad <sherine.awad@gmail.com>
2016-01-17 13:59:22 -0800

    update genome_ivals

--- scripts/nucmer/genome_ivals.py
+++ scripts/nucmer/genome_ivals.py
@@ -31,6 +31,39 @@ def load_sequence(filename):
 	d[record.name.split()[0]] = record.sequence
     return d
 
+def compare(infile1, infile2, outfile): 
+           
+           fout =open(outfile, 'a')
+           refsizes = load_refsizes('mircea.fa')
+           gic1 = GenomeIntervalsContainer(refsizes)
+           gic1.load_coords(infile1)
+
+           gic2 = GenomeIntervalsContainer(refsizes)
+           gic2.load_coords(infile2)
+
+           gic1_uncov_d = gic1.calc_uncov()
+           gic1_total_bp = sum(gic1.refsizes.values())
+           gic1_total_uncov = sum(gic1_uncov_d.values())
+           print >>fout, infile1, gic1_total_bp,  float(gic1_total_uncov) / gic1_total_bp
+
+
+           gic2_uncov_d = gic2.calc_uncov()
+           gic2_total_bp = sum(gic2.refsizes.values())
+           gic2_total_uncov = sum(gic2_uncov_d.values())
+           print >>fout, infile2, gic2_total_bp,  float(gic2_total_uncov) / gic2_total_bp
+
+           intersect_gic12 = gic1.intersect(gic2)
+           print >>fout, 'intersect', gic1_total_bp, gic2_total_bp, sum(intersect_gic12.calc_uncov().values())
+
+           union_gic12 = gic1.union(gic2)
+           print >>fout, 'union', gic1_total_bp, gic2_total_bp, sum(union_gic12.calc_uncov().values())
+ 
+           jaccard_sim12= float(sum(intersect_gic12.calc_uncov().values())) / float (sum(union_gic12.calc_uncov().values()))
+
+           print >>fout, 'Jaccard Similarity between', infile1, 'and',infile2, 'is:', jaccard_sim12
+
+	   fout.close()	   
+
 class GenomeIntervalsContainer(object):
     def __init__(self, refsizes):
         self.names = refsizes.keys()
@@ -112,8 +145,46 @@ class GenomeIntervalsContainer(object):
 
         fout.close()
 
+    def window_slide(self, other, win,  cutoff,n):
+	total_regions1 ={}
+ 	total_regions2 ={}
+	for i in xrange(0,n):
+		total_regions1[i]=0
+		total_regions2[i]=0
+	window=win
+        i =0
+        sum1 =0
+        sum2 =0 
+        index1 =0 
+        index2 =0
+     	for k in self.trees:
+	   while i < len(self.trees[k]):
+		for j in xrange(i,window):
+ 			if self.trees[k][j] ==1.0:
+				sum1+=1 
+                        if other.trees[k][j] ==1.0:
+                                sum2+=1
+		i+=window
+                window +=win
+		if sum1 >= cutoff: 
+			total_regions1[index1]=1.0
+                        index1+=1
+			sum1 =0
+                if sum2 >=cutoff: 
+			total_regions2[index2]=1.0
+			index2+=1 
+			sum2 =0
+        commons =0
+	diff =0
+	for i in xrange (0,len(total_regions1)):
+		print total_regions1[i], total_regions2[i]
+		if total_regions1[i]+total_regions2[i] ==2: 
+			commons+=1
+		elif total_regions1[i]+total_regions2[i] ==1:  
+			 diff+=1
 
-
+	return commons, diff
+		
 def main():
     print 'loading refsizes'
     refsizes = load_refsizes('mircea.fa')
@@ -135,8 +206,8 @@ def main():
 
     gic_sqc = GenomeIntervalsContainer(refsizes)
     gic_sqc.load_coords('sqc.coords')
-   
-    """
+  
+    """ 
     gic_iqc.write_genomes('iqc.cov',reference,1.0)
     gic_iqc.write_genomes('iqc.uncov',reference,0.0)
 
@@ -145,17 +216,15 @@ def main():
 	
     gic_sqc.write_genomes('sqc.cov', reference,1.0) 
     gic_sqc.write_genomes('sqc.uncov', reference,0.0)
-    """ 
+
+     
 
     iqc_uncov_d = gic_iqc.calc_uncov()
-    #for name in iqc_uncov_d:
-        #print name, iqc_uncov_d[name]
+    for name in iqc_uncov_d:
+        print name, iqc_uncov_d[name]
     iqc_total_bp = sum(gic_iqc.refsizes.values())
     iqc_total_uncov = sum(iqc_uncov_d.values())
     print "For IDBA QC:", iqc_total_bp, iqc_total_uncov, float(iqc_total_uncov) / iqc_total_bp 
-
-    mqc_uncov_d = gic_mqc.calc_uncov()
-    mqc_total_bp = sum(gic_mqc.refsizes.values())
 
     sqc_uncov_d = gic_sqc.calc_uncov()
     sqc_total_bp = sum(gic_sqc.refsizes.values())
@@ -175,18 +244,14 @@ def main():
     intersect_is_qc = gic_iqc.intersect(gic_sqc)
     print 'intersect', iqc_total_bp,sqc_total_bp, sum(intersect_is_qc.calc_uncov().values())
 
+    """ 
 
-    im_qc_union_cov = iqc_total_bp - sum(union_im_qc.calc_uncov().values())
-    im_qc_intersect_cov = iqc_total_bp - sum(intersect_im_qc.calc_uncov().values()) 
-    print 'intersect and union cov for iqc and mqc:', im_qc_intersect_cov, im_qc_union_cov
-    print "Jaccard Similarity between IDBA QC and MEGAHIT QC: ", float(im_qc_intersect_cov)/float(im_qc_union_cov)
-
-    is_qc_union_cov = iqc_total_bp - sum(union_is_qc.calc_uncov().values())
-    is_qc_intersect_cov = iqc_total_bp - sum(intersect_is_qc.calc_uncov().values()) 
-    print 'intersect and union cov for iqc and sqc', is_qc_intersect_cov, is_qc_union_cov
-    print "Jaccard Similarity between IDBA QC and SPAdes QC: ", float(is_qc_intersect_cov)/float(is_qc_union_cov)
-
+    #compare('mqc.coords', 'mdigi.coords', 'megahit-qc-digi')
+    #compare('mqc.coords', 'mpart.coords', 'megahit-digi-part') 
     
-    
+    commons,diff= gic_iqc.window_slide(gic_mqc, 100, 90,len(reference) ) 
+    print "Jaccard Similarity =",commons, diff,  float(diff+0.01)/float(commons+0.01)
+
+   
 if __name__ == '__main__':
     main()
