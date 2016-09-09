03bfb7c43bc4d27fc03f85f20bf65e53ee3b4d97
Sherine.Awad <sherine.awad@gmail.com>
2016-09-09 11:14:44 -0400

    Update more scripts

--- scripts/uncovered-coverage.py
+++ scripts/uncovered-coverage.py
@@ -15,13 +15,13 @@ def main():
     parser.add_argument('genome')
     parser.add_argument('samfile1')
     parser.add_argument('uncovered')
-    parser.add_argument('bases') 
-    parser.add_argument('coverage') 
+    parser.add_argument('common') 
+    parser.add_argument('all') 
  
     args = parser.parse_args()
     
-    fout1 = open(args.bases, 'w+') 
-    fout2 = open(args.coverage, 'w+')    
+    fout1 = open(args.common, 'w+') 
+    fout2 = open(args.all, 'w+')    
 
 
     genome_dict1 = {}
@@ -31,24 +31,31 @@ def main():
     for record in screed.open(args.genome):
         genome_dict1[record.name] = [0] * len(record.sequence)
 	n_rec+=len(record.sequence)
-        genome_covered[record.name] = [-1] * len(record.sequence) *4 #4 assemblers commons 
-
+        genome_covered[record.name] = [0] * len(record.sequence) *4 #Without *4 throws an error index out of array size 
+    
     for line in open(args.uncovered):
        if line.startswith('>'): 
-             name =line
-             continue
+             line.split('>') 
+             name = line[1:]
+             name = name.replace("\n", "")
+              
        else: 
-	     bases =line.strip().split(",")
-             for i in bases: 
-		if i =='': 
- 			break 
-                else:
-       			genome_covered[record.name][int(i)]=bases
-   
+	     import pdb; pdb.set_trace()
+             pos =line.strip().split(",")
+             #import pdb; pdb.set_trace()
+	     genome_covered[name][int(i)]= 1
+ 	#	     if name =='Sulfolobus_tokodaii':
+        #       			 continue
+		     #print name, i , genome_covered[name][int(i)]
+    #for name in genome_covered:
+	#for i in genome_covered[name]: 
+        #           print name, i , genome_covered[name][int(i)]  
+    
+    # """
     arr =[0]*n_rec    
     n = 0
     n_skipped = 0
-
+   
     for samline in ignore_at(open(args.samfile1)):
         n += 1
         if n % 100000 == 0:
@@ -72,38 +79,42 @@ def main():
         for i in range(refpos - 1, refpos + len(seq) - 1):
              if i < len(ref):
                 ref[i] += 1
-    #covx = 0 
+    
     for name in genome_dict1: 
-       ref = genome_dict1[name]
-       for j in range ( len(genome_dict1[name])):
-             arr[ref[j]]+=1 
-       #print sum(genome_dict1[name])
-       #x =  sum(genome_dict1[name])
-       #covx += float(x)
-
-
-    #print covx
+             ref = genome_dict1[name]
+             for j in range ( len(genome_dict1[name])):
+                   arr[ref[j]]+=1 
 
    
-    print >>fout2, 'Bases Coverage Distribution'   
+    print >>fout2, 'Uncovered coverage Distribution'   
     for i in range(len(arr)): 
 	print >>fout2,  i, arr[i]
     
     arr1 = []
     arr1 =[0]*n_rec  
+    
+    for name in genome_covered:
+        ref1 = genome_covered[name]
+	for i in range(0, len(genome_covered[name]) ): 
+		if genome_covered[name][i] ==1: 
+			#ref1[i] = genome_dict1[name][i] 
+			print name, genome_dict1[name][i]
+
+    #" 
     for name in genome_dict1:
         ref1 = genome_dict1[name]
         for i in range (len(genome_dict1[name])):
               x = genome_covered[name][i] 
 	      y = genome_dict1[name][i]
-              if (x > -1) : 
+              if (x !=-1) : 
 		  ref1[i] = y
+     #" 
  
 
     for j in range ( len(genome_dict1[name])):
             arr1[ref1[j]]+=1
      
-    print >>fout1, 'Uncovered bases Coverage Distribution'  
+    print >>fout1, 'Common uncovered bases Coverage Distribution'  
     for i in range(len(arr1)):
         if arr1[i] >0:
             print >>fout1, i, arr1[i]
@@ -111,16 +122,15 @@ def main():
     #if n_skipped / float(n) > .01:
         #raise Exception, "Error: too many reads ignored! %d of %d" % \
               #(n_skipped, n)
-
+   
     total = 0.
     cov1 = 0.
-    """ 
+    
     for name in genome_dict1:
         total += len(genome_dict1[name])
         x =  sum(genome_dict1[name])  
         cov1 += float(x)  
     print >> fout1, args.samfile1, float(cov1) / float(total), cov1, total
-    """
-
+    # """ 
 if __name__ == '__main__':
     main()
