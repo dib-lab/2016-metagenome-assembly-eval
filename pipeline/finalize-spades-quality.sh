eef8d5105ad7fa45a670e6d04ff23e882a7119fe
Sherine.Awad <sherine.awad@gmail.com>
2016-11-19 19:48:43 -0500

    remove old stuff

--- pipeline/finalize-spades-quality.sh
+++
@@ -1,2 +0,0 @@
-python /mnt/home/mahmoud4/khmer/sandbox/calc-best-assembly.py SRR606249.spades.d/contigs.fasta -o spades-quality-best.fa
-python /mnt/home/mahmoud4/khmer/sandbox/multi-rename.py assembly spades-quality-best.fa > spades-quality-assembly.fa
--- pipeline/seperate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in kak*.fq
-do
-   extract-paired-reads.py $i
-   name=$(basename $i .fq)
-   mv ${name}.fq.pe ${name}.pe.fq
-   mv ${name}.fq.se ${name}.se.fq
-done
