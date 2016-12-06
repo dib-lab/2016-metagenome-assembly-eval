61966163b897cbc4669faf8f4199efc3d5b8d810
Sherine.Awad <sherine.awad@gmail.com>
2016-12-06 10:25:57 -0500

    update makefile and get-mapped/unmapped and add multirename

---
+++ pipeline/multi-rename.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python2
+#
+# This file is part of khmer, http://github.com/ged-lab/khmer/, and is
+# Copyright (C) Michigan State University, 2009-2013. It is licensed under
+# the three-clause BSD license; see doc/LICENSE.txt.
+# Contact: khmer-project@idyll.org
+#
+import screed
+import sys
+
+CUTOFF = 200
+
+
+def main():
+    n = 0
+    prefix = sys.argv[1]
+    for filename in sys.argv[2:]:
+        for record in screed.open(filename):
+            if len(record.sequence) >= CUTOFF:
+                n += 1
+                print '>%s.%s %s\n%s' % (prefix, n, record.name, record.sequence)
+
+
+if __name__ == '__main__':
+    main()
