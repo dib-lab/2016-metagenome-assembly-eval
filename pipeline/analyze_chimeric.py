4ce8a32546e5e35f3e525c52d8f0908cfc39ba0d
C. Titus Brown <titus@idyll.org>
2017-04-15 14:18:18 -0700

    update a few things

--- pipeline/analyze_chimeric.py
+++ pipeline/analyze_chimeric.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 from collections import defaultdict
+import pprint
 
 # load a .coords file output by nucmer
 def _load_coords(filename, only=None):
@@ -27,24 +28,37 @@ def main():
     parser.add_argument('coords')
     parser.add_argument('-p', '--percent-identity', type=float, default=0.99)
     parser.add_argument('-l', '--min-length', type=int, default=100)
+    parser.add_argument('--eliminate-prefix', type=int, default=None)
     args = parser.parse_args()
 
     matches = defaultdict(set)
     for (s1, e1, s2, e2, ident, name1, name2) in _load_coords(args.coords):
-        if ident >= args.percent_identity and e2-s2 >= args.min_length:
+        if ident >= args.percent_identity and e2 - s2 + 1 >= args.min_length:
             matches[name2].add(name1)
 
     n = 0
     m = 0
+    namecounts = defaultdict(int)
     for k in matches:
         n += 1
 
         if len(matches[k]) > 1:
-            #print(k, matches[k])
-            m += 1
+            # prefix matching
+            if args.eliminate_prefix:
+                s2 = set([ n[:args.eliminate_prefix] for n in matches[k] ])
+                if len(s2) > 1:
+                    #print(k, matches[k], len(s2))
+                    for j in matches[k]:
+                        namecounts[j] += 1
+                    m += 1
+            else:
+                #print(k, matches[k])
+                m += 1
+                for j in matches[k]:
+                    namecounts[j] += 1
 
-    print(n, m)
-
+    print(n, m, float(m) / n)
+    pprint.pprint(sorted(namecounts.items(), key=lambda x: x[1]))
 
 if __name__ == '__main__':
     main()
