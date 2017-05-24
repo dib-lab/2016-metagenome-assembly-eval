f49bcb145ac67ad62a07914f81ea32db745fb37c
Titus Brown <titus@idyll.org>
2017-05-24 10:21:03 -0400

    update assembly stats

--- pipeline/run-analysis-1.sh
+++ pipeline/run-analysis-1.sh
@@ -3,15 +3,16 @@
 #PBS -l walltime=96:00:00
 #PBS -l mem=100GB
 #PBS -N analysis-1
+#PBS -m abe
 
-cd /mnt/research/ged/sherine/2015-metagenome-assembly/pipeline
+cd /mnt/research/ged/ctb/2015-metagenome-assembly/pipeline
 
 module load bwa
 module load samtools
 
 make analysis
-make readcoverage
-make countunmapped 
-make unalignedcoverage
+#make readcoverage
+#make countunmapped 
+#make unalignedcoverage
 
 
